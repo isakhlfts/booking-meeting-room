81dcaab15cac20ed5ae59b57865f4ef254c48b0a
isakhlfts <isakholifatus04@gmail.com>
2023-10-13 07:40:29 +0700

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -24,41 +24,18 @@ if(isset($_POST["signup-submit"])){
 	 }elseif ($password !== $passwordRepeat) {
 	 	header("Location: ../signup.php?error=passwordcheck&uid=".$username."&email=".$email);
 	 	exit();
-	 }else{
-	 	$sql="SELECT uidusers FROM users WHERE uidUsers=?";
-	 	$stmt=mysqli_stmt_init($conn);
-	 	if (!mysqli_stmt_prepare($stmt,$sql)) {
-	 		header("Location: ../signup.php?error=sqlerror");
-	 		exit();
-	 	}else{
-	 		mysqli_stmt_bind_param($stmt,"s",$username);    //one S for one ques mark
-	 		mysqli_stmt_execute($stmt);
-	 		mysqli_stmt_store_result($stmt);
-	 		$resultCheck = mysqli_stmt_num_rows($stmt);
-	 		if ($resultCheck > 0) {
-	 			header("Location: ../signup.php?error=usertaken&email=".$email);
-	 		exit();
-	 		}else{
-	 			$sql = "INSERT INTO users (uidUsers,emailUsers,pwdUsers) VALUES (?,?,?)";
-	 			$stmt=mysqli_stmt_init($conn);
- 				 if (!mysqli_stmt_prepare($stmt,$sql)) {
- 				 	header("Location: ../signup.php?error=sqlerror");
- 				 	exit(); 
-	 			}else{
-	 				$hashedPwd=password_hash($password, PASSWORD_DEFAULT);
+	 }else {
+        $signupSuccess = saveUserToDatabase($username, $email, $password, $passwordRepeat);
+        $signupSuccess = true;
 
-	 				mysqli_stmt_bind_param($stmt,"sss",$username,$email,$hashedPwd);   
-	 				mysqli_stmt_execute($stmt);
-	 				header("Location: ../index.php?signup=success");
- 				 	exit();
-	 			}
-	 		}
-	 	}
-
-	}
-	mysqli_stmt_close($stmt);
-	mysqli_close($conn);
-}else{
-	header("Location: ../signup.php?signup=success");
- 	exit();
+        if ($signupSuccess) {
+            header("Location: home.php");
+            exit();
+        }
+        else {
+            header("Location: signup.php?error=usertaken");
+            exit();
+        }
+    }
 }
+?>

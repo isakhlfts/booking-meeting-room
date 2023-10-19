e3da9646057f9c082d6c37dada57119574ac0628
isakhlfts <isakholifatus04@gmail.com>
2023-10-19 22:31:28 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,5 +1,6 @@
 <?php
 
+session_start();
 if (!isset($_SESSION["admin_logged_in"]) || $_SESSION["admin_logged_in"] !== true) {
     // Admin belum login, alihkan ke halaman login admin
     header("Location: admin_login.php");

be2d97aadc9216ecd2f3829102db2c9094216e2a
isakhlfts <isakholifatus04@gmail.com>
2023-10-19 19:11:31 +0700

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -10,4 +10,27 @@ $conn=mysqli_connect($servername,$dbusername,$password,$dBName);
 if(!$conn){
 	die("Connection failed: ".mysqli_connect_error());
 }
+
+
+// Form Peminjaman
+$nama_peminjam = $_POST['nama_peminjam'];
+$tanggal = $_POST['tanggal'];
+$waktu = $_POST['waktu'];
+$keterangan = $_POST['keterangan'];
+
+// SQL untuk memasukkan data ke tabel
+$sql = "INSERT INTO form (nama_peminjam, tanggal, waktu, keterangan)
+        VALUES ('$nama_peminjam', '$tanggal', '$waktu', '$keterangan')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Peminjaman ruangan berhasil disimpan.";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 ?>
+
+
+
+

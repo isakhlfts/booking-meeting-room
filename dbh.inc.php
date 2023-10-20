64bda3726c055018c9a9fc3e8c227022c18caf3c
isakhlfts <isakholifatus04@gmail.com>
2023-10-20 11:30:46 +0700

    Update dbh.inc.php

--- dbh.inc.php
+++ dbh.inc.php
@@ -1,48 +1,44 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "booking";
 
-$servername="localhost";
-$username="root";
-$password="";
-$database="booking";
-
-$koneksi = new mysqli($servername,$username,$password,$database);
+$koneksi = new mysqli($servername, $username, $password, $database);
 
 if ($koneksi->connect_error) {
     die("Koneksi ke database gagal: " . $koneksi->connect_error);
 }
 
-// Untuk user.php
+// Periksa apakah ada data POST yang dikirimkan
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Mendapatkan data dari formulir
-    $name = $_POST["name"];
-    $date = $_POST["date"];
-    $time = $_POST["time"];
-    $description = $_POST["description"];
+    if (isset($_POST["name"]) && isset($_POST["date"]) && isset($_POST["time"]) && isset($_POST["description"])) {
+        $name = $_POST["name"];
+        $date = $_POST["date"];
+        $time = $_POST["time"];
+        $description = $_POST["description"];
 
-    // Menyiapkan perintah SQL INSERT
-    $sql = "INSERT INTO form (name, date, time, description) VALUES (?,?,?,?)";
+        // Menyiapkan perintah SQL INSERT
+        $sql = "INSERT INTO form (name, date, time, description) VALUES (?,?,?,?)";
 
-    // Mempersiapkan statement SQL dengan menggunakan prepared statement
-    $stmt = $koneksi->prepare($sql);
-    if ($stmt) {
-        $stmt->bind_param("ssss", $name, $date, $time, $description);
-        if ($stmt->execute()) {
-            echo '<script>alert("Formulir terkirim!");</script>';
-            echo '<script>window.location.href = "index.php";</script>';
-            exit;
+        // Mempersiapkan statement SQL dengan menggunakan prepared statement
+        $stmt = $koneksi->prepare($sql);
+        if ($stmt) {
+            $stmt->bind_param("ssss", $name, $date, $time, $description);
+            if ($stmt->execute()) {
+                echo '<script>alert("Formulir terkirim!");</script>';
+                echo '<script>window.location.href = "index.php";</script>';
+                exit;
+            } else {
+                echo "Kesalahan saat menyimpan data: " . $stmt->error;
+            }
+
+            // Menutup statement
+            $stmt->close();
         } else {
-            echo "Kesalahan saat menyimpan data: " . $stmt->error;
+            echo "Kesalahan dalam pernyataan SQL: " . $koneksi->error;
         }
-
-        // Menutup statement
-        $stmt->close();
-    } else {
-        echo "Kesalahan dalam pernyataan SQL: " . $koneksi->error;
     }
 }
 
 ?>
-
-
-
-

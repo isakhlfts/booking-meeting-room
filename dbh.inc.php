6a7e9f4e9efc53132bb0a4b46edbe9b9035d5a65
isakhlfts <isakholifatus04@gmail.com>
2023-10-20 07:36:26 +0700

    Create dbh.inc.php

---
+++ dbh.inc.php
@@ -0,0 +1,52 @@
+<?php
+
+$servername="localhost";
+$username="root";
+$password="";
+$database="booking";
+
+$koneksi = new mysqli($servername,$username,$password,$database);
+
+if ($koneksi->connect_error) {
+    die("Koneksi ke database gagal: " . $koneksi->connect_error);
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Mendapatkan data dari formulir
+    $name = $_POST["name"];
+    $date = $_POST["date"];
+    $time = $_POST["time"];
+    $description = $_POST["description"];
+
+    // Menyiapkan perintah SQL INSERT
+    $sql = "INSERT INTO form (name, date, time, description) VALUES (?,?,?,?)";
+
+    // Mempersiapkan pernyataan SQL dengan menggunakan prepared statement
+    $stmt = $koneksi->prepare($sql);
+
+    if ($stmt) {
+        // Mengikat parameter ke pernyataan
+        $stmt->bind_param("ssss", $name, $date, $time, $description);
+
+        // Menjalankan pernyataan SQL
+        if ($stmt->execute()) {
+            echo '<script>alert("Formulir terkirim!");</script>';
+            echo '<script>window.location.href = "home.php";</script>';
+            exit;
+        } else {
+            echo "Kesalahan saat menyimpan data: " . $stmt->error;
+        }
+
+        // Menutup pernyataan
+        $stmt->close();
+    } else {
+        echo "Kesalahan dalam pernyataan SQL: " . $koneksi->error;
+    }
+}
+
+?>
+
+
+
+
